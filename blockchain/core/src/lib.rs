b4f78868e73c399208155b78f941ac59c91fa7b4
vinicruz07 <cruzvinicius2007@gmail.com>
2026-09-16 20:20:58 -0300

    Adicionando função para calcular o hash

--- blockchain/core/src/lib.rs
+++ blockchain/core/src/lib.rs
@@ -1,10 +1,24 @@
+/// Módulo core responsável pelas estruturas de dados do blockchain
+/// Escrito em no_std para rodar bare metal
 #![no_std]
 
-/// Variavel publica de hash
+/// Dependencias externas
+/// * sha2 - Algoritmo de hash SHA-256
+use sha2::{Digest, Sha256};
+
+/// Variavel publica do tipo hash
 /// Um hash SHA-256 é composto por exatamente 32 bytes brutos (256 bits).
-/// pub type Hash = [u8; 32];
 pub type Hash = [u8; 32];
 
+/// Struct de cabecalho de bloco
+///
+/// # Fields
+///
+/// * `index` - Numero do bloco na blockchain
+/// * `timestamp` - Timestamp quando o bloco foi criado (em segundos desde a epoch)
+/// * `prev_hash` - Hash do bloco anterior
+/// * `data_hash` - Hash dos dados do bloco
+/// * `nonce` - Numero usado para encontrar um hash valido
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct BlockHeader {
     pub index: u32,
@@ -14,7 +28,10 @@ pub struct BlockHeader {
     pub nonce: u64,
 }
 
+///Funcoes da struct BlockHeader
 impl BlockHeader {
+
+    /// Funcao new para instanciar um BlockHeader
     pub fn new(index: u32, timestamp: u64, prev_hash: Hash, data_hash: Hash) -> Self {
         Self {
             index,
@@ -23,5 +40,25 @@ impl BlockHeader {
             data_hash,
             nonce: 0,
         }
+    }
+
+    /// Funcao que serializa o header do bloco e calcula o hash
+    /// 1. Inicializa o estado interno do hasher SHA-256
+    /// 2. Alimenta o hasher em Little-Endian para garantir um hash determinístico cross-platform (x86, ARM, ESP32)
+    /// 3. Finaliza a computação e converte o resultado no tipo `Hash` ([u8; 32])
+    pub fn calc_hash(&self) -> Hash {
+        
+        // 1. Inicializa o estado interno do hasher SHA-256
+        let mut hasher = Sha256::new();
+
+        // 2. Alimenta o hasher garantindo um hash determinístico cross-platform (x86, ARM, ESP32)
+        hasher.update(self.index.to_be_bytes());
+        hasher.update(self.timestamp.to_be_bytes());
+        hasher.update(&self.prev_hash);
+        hasher.update(&self.data_hash);
+        hasher.update(self.nonce.to_be_bytes());
+
+        // 3. Finaliza a computação e converte o resultado no tipo `Hash` ([u8; 32])
+        hasher.finalize().into()
     }
 }
